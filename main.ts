f0110c3deaefe3e89334961e08d9814084d97cd5
dcerver <121509667+dcerver@users.noreply.github.com>
2023-01-22 22:42:13 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ OLED.init(128, 64)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("your_ssid", "your_pwd")
 basic.forever(function () {
-    OLED.writeString("Dust ug/m3: ")
+    OLED.writeString("Pols ug/m3: ")
     OLED.writeNum(Environment.ReadDust(DigitalPin.P16, AnalogPin.P1))
     OLED.newLine()
     OLED.writeString("Temperatura C: ")
